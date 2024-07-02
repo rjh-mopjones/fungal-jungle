b8e87dae3dd1bed1f7b453ba443045e0fa763247
rjh-mopjones <roryhedderman@gmail.com>
2024-07-02 21:40:24 +0100

    add in bevy-ecs tilemap to generate tilemap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use bevy::math::vec3;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::log::{Level, LogPlugin};
 use bevy::prelude::*;
-use bevy::prelude::Color::Rgba;
+use bevy_ecs_tilemap::prelude::*;
 use bevy::window::close_on_esc;
 use bevy_pancam::{PanCam, PanCamPlugin};
-use crate::macro_map::macro_map::write_macro_map_to_file;
 
 pub mod jungle_noise;
 mod macro_map;
@@ -20,8 +20,20 @@ const MAP_WIDTH: usize = 1024;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(DefaultPlugins
+             .set(ImagePlugin::default_nearest())
+             .set(LogPlugin {
+                 // Everything else should be set to Level::ERROR
+                 level: Level::ERROR,
+                 // except for bevy_ecs_tilemap
+                 filter: "bevy_ecs_tilemap=trace".into(),
+                 ..default()
+             }),
+        )
+        .add_plugins(TilemapPlugin)
         .add_plugins(PanCamPlugin)
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_plugins(LogDiagnosticsPlugin::default())
         .add_systems(Startup, setup)
         .add_systems(Update, close_on_esc)
         .run();
@@ -30,41 +42,43 @@ fn main() {
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
     commands.spawn(Camera2dBundle::default()).insert(PanCam::default());
-    // let texture = asset_server.load(SPRITE_SHEET_PATH);
-    // let layout =
-    //     TextureAtlasLayout::from_grid(Vec2::new(TILE_W as f32, TILE_H as f32), 7, 1, None, None);
-    // let texture_atlas_layout = texture_atlas_layouts.add(layout);
-    let image_macro_map = jungle_noise::tidal::generate_in_house_tidal_noise(MAP_WIDTH, MAP_HEIGHT, 42);
-    write_macro_map_to_file(image_macro_map, "macro-map-tidally-locked.png");
     let macro_map = jungle_noise::tidal::generate_in_house_tidal_noise(MAP_WIDTH, MAP_HEIGHT, 42);
 
-    for line in macro_map.map {
-        for tile in line.map {
-            let (x, y) = grid_to_world(tile.coords.0 as f32, tile.coords.1 as f32);
-            commands.spawn(
-                (SpriteBundle{
-                    sprite: Sprite{
-                        color: tile.tile.colour(),
-                        flip_x: false,
-                        flip_y: false,
-                        custom_size: Option::from(Vec2::new(1.0, 1.0)),
-                        rect: None,
-                        anchor: Default::default(),
-                    },
-                    transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR as f32))
-                        .with_translation(vec3(x, y, 0.0)),
-                    ..default()
-                }),
-            );
+    let texture_handle: Handle<Image> = asset_server.load("one-tile.png");
+    let tilemap_entity = commands.spawn_empty().id();
+    let map_size = TilemapSize { x: MAP_WIDTH as u32, y: MAP_HEIGHT as u32 };
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    texture_index: TileTextureIndex(0),
+                    color: TileColor::from(macro_map.map[y as usize].map[x as usize].tile.colour()),
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
         }
     }
-    fn grid_to_world(x : f32, y :f32) -> (f32, f32) {
-        (
-            x * TILE_W as f32,
-            y * TILE_H as f32
-        )
-    }
+
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::Square;
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        texture: TilemapTexture::Single(texture_handle),
+        storage: tile_storage,
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
 }
