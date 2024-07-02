b2eba8bdf718c91ade731980b30e8c27e0386004
rjh-mopjones <roryhedderman@gmail.com>
2024-07-02 15:01:19 +0100

    display a sprite with bevy

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
-use std::time::Instant;
-use noise::{MultiFractal, Seedable};
-use noise::utils::{NoiseMapBuilder};
-use crate::macro_map::macro_map::write_macro_map_to_file;
+use bevy::{
+    prelude::*,
+};
+use bevy::window::close_on_esc;
 
-pub mod io;
-pub mod jungle_noise;
-mod macro_map;
+const SPRITE_SHEET_PATH: &str = "sprite-sheet.png";
+const TILE_W: usize = 6;
+const TILE_H: usize = 8;
 
 fn main() {
-    let start = Instant::now();
-    let macro_map = jungle_noise::tidal::generate_in_house_tidal_noise(1024, 512, 42);
-    let duration = start.elapsed();
-    println!("Time elapsed: {:?}", duration);
-    write_macro_map_to_file(macro_map, "macro-map-tidally-locked.png");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, close_on_esc)
+        .run();
 }
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    let texture = asset_server.load(SPRITE_SHEET_PATH);
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(TILE_W as f32, TILE_H as f32), 7, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    commands.spawn((
+        SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 2,
+            },
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        }
+    ));}
