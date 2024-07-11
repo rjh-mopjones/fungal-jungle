a9b4467c89855d0f40f7dcbabc304f969be2c9c0
rjh-mopjones <roryhedderman@gmail.com>
2024-07-11 20:00:23 +0100

    added in world inspector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::default::Default;
+use bevy::prelude::Name;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::input::common_conditions::input_toggle_active;
 use bevy::prelude::*;
 use bevy::render::render_asset::RenderAssetUsages;
 use bevy_ecs_tilemap::map::{TilemapId, TilemapTexture, TilemapType};
 use bevy_ecs_tilemap::prelude::{get_tilemap_center_transform, TileBundle, TilemapSize, TilemapTileSize, TilePos, TileStorage, TileTextureIndex};
 use bevy_ecs_tilemap::{TilemapBundle, TilemapPlugin};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use image::imageops::FilterType;
 use crate::engine::pancam::lib::{PanCam, PanCamPlugin};
 use crate::macro_map::macro_map::{write_meso_map_to_file};
@@ -29,6 +33,7 @@ fn main() {
              .set(ImagePlugin::default_nearest())
         )
         .add_plugins(PanCamPlugin)
+        .add_plugins(WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)))
         .add_plugins(TilemapPlugin)
         .add_plugins(FrameTimeDiagnosticsPlugin)
         .add_plugins(LogDiagnosticsPlugin::default())
@@ -63,28 +68,30 @@ fn render_terrain(mut commands: &mut Commands,
 ) {
     let macro_map = jungle_noise::tidal::generate_in_house_tidal_noise(MAP_WIDTH, MAP_HEIGHT, 42);
     write_meso_map_to_file(macro_map.clone(), 10, "meso-map.png");
-    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
-    let tilemap_entity = commands.spawn_empty().id();
+    let macro_map_entity = commands.spawn(Name::new("MacroMap")).id();
     let map_size = TilemapSize { x: (MAP_WIDTH / 16) as u32, y: (MAP_HEIGHT / 16) as u32 };
 
     let mut tile_storage = TileStorage::empty(map_size);
     let mut meso_map_images: Vec<Handle<Image>> = vec![];
+    let mut meso_map_entites: Vec<Entity> = vec![];
 
     for (i, meso_map) in macro_map.meso_maps.iter().enumerate() {
             let tile_pos = TilePos { x: meso_map.index.x as u32, y: map_size.y - 1 - meso_map.index.y as u32};
             let tile_entity = commands
-                .spawn(TileBundle {
+                .spawn((TileBundle {
                     position: tile_pos,
                     texture_index: TileTextureIndex(i as u32),
-                    tilemap_id: TilemapId(tilemap_entity),
+                    tilemap_id: TilemapId(macro_map_entity),
                     ..Default::default()
-                })
+                }, Name::new(format!("MesoMap{}",meso_map.index))))
                 .id();
+            meso_map_entites.push(tile_entity);
             tile_storage.set(&tile_pos, tile_entity);
+
         meso_map_images.push(images.add(
             Image::from_dynamic(meso_map.low_res_dynamic_image.clone()
                                     .fliph()
-                                    .resize(64, 64, FilterType::Nearest), false, RenderAssetUsages::default())
+                                    .resize(64, 64, FilterType::Nearest),false, RenderAssetUsages::default())
         ));
     }
 
@@ -92,7 +99,7 @@ fn render_terrain(mut commands: &mut Commands,
     let grid_size = tile_size.into();
     let map_type = TilemapType::Square;
 
-    commands.entity(tilemap_entity).insert(TilemapBundle {
+   commands.entity(macro_map_entity).insert(TilemapBundle {
         grid_size,
         map_type,
         size: map_size,
@@ -101,5 +108,5 @@ fn render_terrain(mut commands: &mut Commands,
         tile_size,
         transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
         ..Default::default()
-    });
+    }).push_children(&*meso_map_entites);
 }
